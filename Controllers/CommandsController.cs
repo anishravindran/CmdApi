34a7819e46f5da3d68ebdf66f6a4c258a6d79ffe
Anish Ravindran <ravindran.anish@gmail.com>
2021-10-30 16:13:05 +0530

    Added delete and update methods

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using CmdApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -31,6 +32,40 @@ namespace CmdApi.Controllers
                 return NotFound();
             }
             return command;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Command>> CreateCommand(Command command)
+        {
+            if (command != null)
+            {
+                _context.CommandItems.Add(command);
+                await _context.SaveChangesAsync();
+            }
+
+            return CreatedAtAction("GetCommandItem", new Command { Id = command.Id }, command);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, Command command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(command).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        {
+            var command = _context.CommandItems.Find(id);
+            _context.Entry(command).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
